baefe4b7ff47845476646264ed3157f7fb13a47e
vezixig <vezixig@googlemail.com>
2023-05-30 20:04:43 +0200

    Kyu 8 - Calculate Averade

---
+++ CodeWars/Kyu8/CalculateAverage.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+/*
+Write a function which calculates the average of the numbers in a given list.
+Note: Empty arrays should return 0.
+ */
+
+namespace CodeWars.Kyu8
+{
+    internal class CalculateAverage
+    {
+        public static double FindAverage(double[] numbers)
+            => numbers.Any() ? numbers.Average() : 0;
+    }
+
+    [TestFixture]
+    public class CalculateAverageTests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            double[] array = new double[] { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 };
+            Assert.AreEqual(200.0 / 13.0, CalculateAverage.FindAverage(array));
+
+            // Should return 0 on empty array
+            Assert.AreEqual(0, CalculateAverage.FindAverage(new double[] { }));
+        }
+    }
+}
